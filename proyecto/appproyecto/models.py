b416af4a812d89f81f0987cd7ffc6f51fa0ceee0
andreina <andrepalm8@gail.com>
2013-10-20 17:30:20 -0430

    proyeto modelo arreglado

--- proyecto/appproyecto/models.py
+++ proyecto/appproyecto/models.py
@@ -5,7 +5,6 @@ import datetime
 
 # Create your models here.
 class usuario(models.Model):
-	usu_id			= models.CharField(max_length=100)
 	usu_nombre		= models.CharField(max_length=50)
 	usu_apellido		= models.CharField(max_length=50)
 	usu_correo		= models.CharField(max_length=50)
@@ -18,71 +17,82 @@ class usuario(models.Model):
 	usu_privacidad		= models.CharField(max_length=50)
 	usu_foto		= models.ImageField(upload_to='imagenusuario')
 	usu_status		= models.BooleanField(default=True)
+	usu_fkusuario		= models.ForeignKey(usuario)
 	def __str__(self):
 		return self.usu_nombre
 
 
 class album(models.Model):
-	alb_id			= models.CharField(max_length=100)
 	alb_nombre		= models.CharField(max_length=100)
 	alb_descripcion		= models.CharField(max_length=200)
 	alb_privacidad		= models.CharField(max_length=200)
 	alb_foto		= models.BooleanField(default=True)
+	alb_fkusuario		= models.ForeignKey(usuario)
+	alb_fkcalendario	= models.ForeignKey(calendario)
 	def __str__(self):
 		return self.alb_nombre
 
 
 class notificacion(models.Model):
-	not_id		= models.CharField(max_length=100)
+	not_fkusuarioemisor		= models.ForeignKey(usuario)
+	not_fkusuarioreceptor	= models.ForeignKey(usuario)
+	not_fkalbum				= models.ForeignKey(album)
+	not_fknotificacion		= models.ForeignKey(notificacion)
+	not_fkinstagram			= models.ForeignKey(instagram)
+	not_fkyoutube			= models.ForeignKey(youtube)
+	not_fksoundcloud		= models.ForeignKey(soundcloud)
+	not_fkcalendario		= models.ForeignKey(calendario)
 	def __str__(self):
 		return self.not_id
 
 class comentario(models.Model):
-	com_id			= models.CharField(max_length=100)
-	com_descripcion		= models.CharField(max_length=200)
+	com_descripcion			= models.CharField(max_length=200)
+	com_fkusuarioemisor		= models.ForeignKey(usuario)
+	com_fkusuarioreceptor	= models.ForeignKey(usuario)
+	com_fkalbum				= models.ForeignKey(album)
+	com_fkcomentario		= models.ForeignKey(comentario)
+	com_fkinstagram			= models.ForeignKey(instagram)
+	com_fkyoutube			= models.ForeignKey(youtube)
+	com_fksoundcloud		= models.ForeignKey(soundcloud)
+	com_fkcalendario		= models.ForeignKey(calendario)
+	
 	def __str__(self):
 		return self.com_descripcion
 
 class megusta(models.Model):
-	mgu_id			= models.CharField(max_length=100)
 	mgu_like		= models.CharField(max_length=200)
+	mgu_fkusuarioemisor		= models.ForeignKey(usuario)
+	mgu_fkusuarioreceptor	= models.ForeignKey(usuario)
+	mgu_fkalbum				= models.ForeignKey(album)
+	mgu_fkinstagram			= models.ForeignKey(instagram)
+	mgu_fkyoutube			= models.ForeignKey(youtube)
+	mgu_fksoundcloud		= models.ForeignKey(soundcloud)
+	mgu_fkcalendario		= models.ForeignKey(calendario)
+
 	def __str__(self):
 		return self-mgu_like
 class calendario(models.Model):
-	cal_id			= models.CharField(max_length=100)
 	cal_fecha		= models.DateTimeField('date published')
 	def __str__(self):
 		return self.cal_fecha
 
 class instagram(models.Model):
-	ins_id			= models.CharField(max_length=100)
 	ins_url			= models.CharField(max_length=200)
+	album			= models.ManyToManyField(album)
 	def __str__(self):
 		return self.ins_url
 
 class soundcloud(models.Model):
-	sou_id			= models.CharField(max_length=100)
 	sou_url			= models.CharField(max_length=200)
+	album			= models.ManyToManyField(album)
 	def __str__(self):
 		return self.sou_url
 
 class youtube(models.Model):
-	you_id			= models.CharField(max_length=100)
 	you_url			= models.CharField(max_length=200)
+	album			= models.ManyToManyField(album)
 	def __str__(self):
 		return self.you_url
 
-class album_instagram(models.Model):
-	albins_id		= models.CharField(max_length=100)
-	def __str__(self):
-		return self.albins_id
 
-class album_soundcloud(models.Model):
-	albsou_id		= models.CharField(max_length=100)
-	def __str__(self):
-		return self.albsou_id
 
-class album_youtube(models.Model):
-	albyou_id		= models.CharField(max_length=100)
-	def __str__(self):
-		return self.albyou_id
