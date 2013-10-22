0ba3211165f19f7801404a5f1d2bdb7fe9e76cd3
emily <emilyfpg@gmail.com>
2013-10-22 08:51:11 -0430

    Subiendo nueva version

--- appproyecto/models.py
+++ appproyecto/models.py
@@ -5,52 +5,73 @@ import datetime
 
 # Create your models here.
 class usuario(models.Model):
-	usu_nombre		= models.CharField(max_length=50)
+	usu_nombre		    = models.CharField(max_length=50)
 	usu_apellido		= models.CharField(max_length=50)
-	usu_correo		= models.CharField(max_length=50)
+	usu_correo		    = models.CharField(max_length=50)
 	usu_nombreUsuario	= models.CharField(max_length=50)
-	usu_clave		= models.CharField(max_length=50)
+	usu_fechanacimiento	= models.CharField(max_length=50)
+	usu_clave		    = models.CharField(max_length=50)
 	usu_direccion		= models.CharField(max_length=100)
-	usu_twitter		= models.CharField(max_length=50)
+	usu_twitter		    = models.CharField(max_length=50)
 	usu_facebook		= models.CharField(max_length=50)
-	usu_google		= models.CharField(max_length=50)
+	usu_google		    = models.CharField(max_length=50)
 	usu_privacidad		= models.CharField(max_length=50)
-	usu_foto		= models.ImageField(upload_to='imagenusuario')
-	usu_status		= models.BooleanField(default=True)
-	usu_fkusuario		= models.ForeignKey(usuario)
+	usu_foto		    = models.ImageField(upload_to='imagenusuario')
+
 	def __str__(self):
 		return self.usu_nombre
 
+class relacionUsuario(models.Model):
+	cal_fecha		= models.ManyToManyField(usuario)
+
+	def __str__(self):
+		return self.cal_fecha
+
+
+class calendario(models.Model):
+	cal_fecha		= models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.cal_fecha
+
+
+class instagram(models.Model):
+	ins_url			= models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.ins_url
+
+
+class soundcloud(models.Model):
+	sou_url			= models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.sou_url
+
+
+class youtube(models.Model):
+	you_url			= models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.you_url
+
 
 class album(models.Model):
-	alb_nombre		= models.CharField(max_length=100)
+	alb_nombre		    = models.CharField(max_length=100)
 	alb_descripcion		= models.CharField(max_length=200)
 	alb_privacidad		= models.CharField(max_length=200)
-	alb_foto		= models.BooleanField(default=True)
+	alb_foto		    = models.BooleanField(default=True)
 	alb_fkusuario		= models.ForeignKey(usuario)
 	alb_fkcalendario	= models.ForeignKey(calendario)
+
 	def __str__(self):
 		return self.alb_nombre
 
 
-class notificacion(models.Model):
-	not_fkusuarioemisor		= models.ForeignKey(usuario)
-	not_fkusuarioreceptor	= models.ForeignKey(usuario)
-	not_fkalbum				= models.ForeignKey(album)
-	not_fknotificacion		= models.ForeignKey(notificacion)
-	not_fkinstagram			= models.ForeignKey(instagram)
-	not_fkyoutube			= models.ForeignKey(youtube)
-	not_fksoundcloud		= models.ForeignKey(soundcloud)
-	not_fkcalendario		= models.ForeignKey(calendario)
-	def __str__(self):
-		return self.not_id
-
 class comentario(models.Model):
 	com_descripcion			= models.CharField(max_length=200)
-	com_fkusuarioemisor		= models.ForeignKey(usuario)
-	com_fkusuarioreceptor	= models.ForeignKey(usuario)
+	com_fkemisor	 		= models.ForeignKey(usuario)
 	com_fkalbum				= models.ForeignKey(album)
-	com_fkcomentario		= models.ForeignKey(comentario)
 	com_fkinstagram			= models.ForeignKey(instagram)
 	com_fkyoutube			= models.ForeignKey(youtube)
 	com_fksoundcloud		= models.ForeignKey(soundcloud)
@@ -59,10 +80,10 @@ class comentario(models.Model):
 	def __str__(self):
 		return self.com_descripcion
 
-class megusta(models.Model):
+
+class meGusta(models.Model):
 	mgu_like		= models.CharField(max_length=200)
-	mgu_fkusuarioemisor		= models.ForeignKey(usuario)
-	mgu_fkusuarioreceptor	= models.ForeignKey(usuario)
+	mgu_fkemisor	= models.ForeignKey(usuario)
 	mgu_fkalbum				= models.ForeignKey(album)
 	mgu_fkinstagram			= models.ForeignKey(instagram)
 	mgu_fkyoutube			= models.ForeignKey(youtube)
@@ -70,29 +91,27 @@ class megusta(models.Model):
 	mgu_fkcalendario		= models.ForeignKey(calendario)
 
 	def __str__(self):
-		return self-mgu_like
-class calendario(models.Model):
-	cal_fecha		= models.DateTimeField('date published')
+		return self.mgu_like
+
+class notificacion(models.Model):
+	not_fkemisor			= models.ForeignKey(usuario)
+	not_fkalbum				= models.ForeignKey(album)
+	not_fkinstagram			= models.ForeignKey(instagram)
+	not_fkyoutube			= models.ForeignKey(youtube)
+	not_fksoundcloud		= models.ForeignKey(soundcloud)
+	not_fkcalendario		= models.ForeignKey(calendario)
 	def __str__(self):
-		return self.cal_fecha
+		return self.not_id
 
-class instagram(models.Model):
-	ins_url			= models.CharField(max_length=200)
-	album			= models.ManyToManyField(album)
+class relacionNotificacion(models.Model):
+	not_fknotificacion		= models.ManyToManyField(notificacion)
+
 	def __str__(self):
-		return self.ins_url
+		return self.mgu_like
 
-class soundcloud(models.Model):
-	sou_url			= models.CharField(max_length=200)
-	album			= models.ManyToManyField(album)
+class relacionComentario(models.Model):
+	not_fkusuarioemisor		= models.ManyToManyField(comentario)
+
 	def __str__(self):
-		return self.sou_url
-
-class youtube(models.Model):
-	you_url			= models.CharField(max_length=200)
-	album			= models.ManyToManyField(album)
-	def __str__(self):
-		return self.you_url
-
-
+		return self.mgu_like
 
