50a3c04635555adeea7ec18c9710c3cc44edf3af
jamaldt <carldeltoro@gmail.com>
2013-10-24 07:04:35 -0430

    subiendo plantilla registro nuevo usuario

--- appproyecto/forms.py
+++ appproyecto/forms.py
@@ -4,5 +4,17 @@ from django import forms
 from appproyecto.models import usuario
 
 class registroForm(ModelForm):
-    class Meta:
-        model = usuario
+	usu_nombreUsuario = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Nombre Usuario', 'class':'inputbox name'}))
+        usu_clave = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Password', 'class':'inputbox password'}))
+        usu_nombre = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Nombre', 'class':'inputbox name'}))
+        usu_apellido = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Apellido', 'class':'inputbox name'}))
+        usu_correo = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Correo Electronico', 'class':'inputbox email'}))
+        usu_direccion = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'Ubicacion Actual', 'class':'inputbox name'}))
+        usu_twitter = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'twitter', 'class':'inputbox email'}))
+        usu_facebook = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'facebook', 'class':'inputbox email'}))
+        usu_google = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'google +', 'class':'inputbox email'}))
+        usu_privacidad = forms.CharField(label='', max_length=20,widget=forms.TextInput(attrs={'placeholder':'privacidad', 'class':'inputbox name'}))
+ 
+ 
+	class Meta:
+		model = usuario
